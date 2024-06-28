d948932584bb2f1117de2bd9dc18ada4332c72b0
RaghvendraUnada <raghvendra@unada.io>
2024-06-28 18:35:32 +0530

    project uploaded

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import Confetti from "react-confetti";
+import HelloGif from "./hello-wave.gif";
 import "./App.css";
 
 const Model = () => {
@@ -36,7 +37,56 @@ function App() {
   }, []);
 
   return (
-    <div className="App" style={{ height: "100vh", width: "100vw" }}>
+    <div
+      className="App"
+      style={{ height: "100vh", width: "100vw", position: "relative" }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "14%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+
+          width: "400px",
+          height: "220px",
+          zIndex: 1000,
+          borderRadius: "10px",
+        }}
+      >
+        <img
+          src={HelloGif}
+          alt="Hello, Wave!"
+          className="hello-wave"
+          style={{
+            width: "130px",
+          }}
+        />
+        <div
+          style={{
+            display: "flex",
+            alignContent: "center",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            width: "300px",
+            margin: "auto",
+          }}
+        >
+          <h3
+            style={{
+              fontFamily: "Poppins",
+              fontSize: "30px",
+              background: "-webkit-linear-gradient(#fd7ecc, #F6289B)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            }}
+          >
+            Hello Khyati
+          </h3>
+          🌺
+        </div>
+      </div>
+
       <Canvas camera={{ position: [0, 0, 20] }}>
         <ambientLight intensity={1.5} />
         <pointLight position={[10, 10, 10]} intensity={1.5} />
